1d0316a6417a3dd75a7712288d9257148f8c8c4c
haroldavis <haroldavis91@gmail.com>
2019-06-09 02:44:27 -0500

    create component for App

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -1,8 +1,28 @@
 import React from 'react';
 
 const About = () => (
-    <h1>Hello World, it is fantastic</h1>
-    
-)
+    <div className="about">
+        <div className="about-container">
+            <div className="about-avatar">
+                <figure>
+                    <img src="" alt="" />
+                </figure>
+            </div>
+            <div className="about-name">
+                <h2>Harold Carhuancho</h2>
+            </div>
+            <div className="about-profession">
+                <p>Fishing Engineer and Fullstack Developer</p>
+            </div>
+            <div className="about-description">
+                <p>I'm dreamer </p>
+            </div>
+            <div className="about-social">
+                <figure></figure>
+            </div>
+        </div>
+    </div>
+
+);
 
 export default About;
---
+++ src/containers/app.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import Main from '../components/main';
+import Sidebar from '../components/sidebar';
+import Info from '../components/info';
+import About from '../components/about';
+import Education from '../components/education';
+import Experience from '../components/experience';
+import Certificates from '../components/certificates';
+import Skills from '../components/skills';
+import Social from '../components/social';
+
+const app = () => {
+    return (
+        <Main>
+            <Sidebar>
+                <About />
+            </Sidebar>
+            <Info>
+                <Experience />
+                <Education />
+                <Certificates />
+            </Info>
+        </Main>
+    );
+}
