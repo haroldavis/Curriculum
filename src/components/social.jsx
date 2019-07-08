696344e7fe32fa00958da3280fbee556e047fd7e
haroldavis <haroldavis91@gmail.com>
2019-07-08 02:03:54 -0500

    work components with styled-components and i make animation in skill component, social component; there are still errors in social component

--- src/components/social.jsx
+++ src/components/social.jsx
@@ -1,19 +1,72 @@
 import React from 'react';
+import styled, { ThemeProvider } from 'styled-components';
+
+
+const SocialStyles = styled.div`
+margin: 0 auto;
+display: block;
+`;
+
+const SocialUl = styled.ul`
+ list-style-type: none;
+ margin: 0;
+ padding: 0;
+`;
+
+const SocialLi = styled.li`
+display: inline;
+margin: 0 1em 0 0;
+`;
+
+const SocialAnchor = styled.a`
+color: #212121;
+text-decoration: none;
+font-size: 1.2em;
+`;
+
+const SocialIcon = styled.i`
+color: ${props => props.theme.color};
+`;
+
+const facebook = {
+    color: "#3b5998"
+}
+
+const twiter = {
+    color: "#38A1F3"
+}
+
+const linkedin = {
+    color: "0e76a8"
+}
+
+const github = {
+    color: "#333"
+}
+
+const getColor = (name) => {
+if (name == "facebook") return facebook;
+if (name == "twiter") return twiter;
+if (name == "linkedin") return linkedin;
+if (name = "github") return github;
+}
 
 const Social = props => (
-    <div className="social">
+    <SocialStyles>
         {props.social &&
-            <ul>
+            <SocialUl>
                 {props.social.map((item, index) => (
-                <li key={`Social-${index}`} target="_blank">
-                    <a href={item.url}>
-                        {item.name}
-                    </a>
-                </li>
+                    <SocialLi key={`Social-${index}`} target="_blank">
+                        <SocialAnchor href={item.url}>
+                            <ThemeProvider theme={getColor(item.name)}>
+                                <SocialIcon className={`fa fa-${item.name}`} />
+                            </ThemeProvider>
+                        </SocialAnchor>
+                    </SocialLi>
                 ))}
-            </ul>
+            </SocialUl>
         }
-    </div>
+    </SocialStyles>
 );
 
 export default Social;
--- src/containers/app.jsx
+++ src/containers/app.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { createGlobalStyle } from 'styled-components';
 
 import Main from '../components/main';
 import Sidebar from '../components/sidebar';
@@ -8,39 +9,50 @@ import Education from '../components/education';
 import Experience from '../components/experience';
 import Certificates from '../components/certificates';
 import Skills from '../components/skills';
-import Social from '../components/social';
 import useGetDate from '../hooks/useGetDate';
+
+const GlobalStyle = createGlobalStyle`
+body {
+    font-family: 'Lato', sans-serif;
+    margin: 0;
+    padding: 0;
+    background: #f5f5f5;
+}
+`;
 
 const App = () => {
     const date = useGetDate();
     console.log(date)
     return date.length === 0 ? <h1>Cargando...</h1> : (
-        <Main> 
-            <Sidebar>
-                <About 
-                avatar={date.avatar}
-                name={date.name}
-                profession={date.profession}
-                bio={date.bio}
-                address={date.address}
-                social={date.social}
-                />
-            </Sidebar>
-            <Info>
-                <Experience 
-                date={date.experience}
-                />
-                <Education 
-                date={date.education}
-                />
-                <Certificates 
-                date={date.certificate}
-                />
-                <Skills
-                date={date.skills} />
-                
-            </Info>
+
+        <Main>
+            <GlobalStyle />
+                <Sidebar>
+                    <About
+                        avatar={date.avatar}
+                        name={date.name}
+                        profession={date.profession}
+                        bio={date.bio}
+                        address={date.address}
+                        social={date.social}
+                    />
+                </Sidebar>
+                <Info>
+                    <Experience
+                        date={date.experience}
+                    />
+                    <Education
+                        date={date.education}
+                    />
+                    <Certificates
+                        date={date.certificate}
+                    />
+                    <Skills
+                        date={date.skills} />
+
+                </Info>            
         </Main>
+
     );
 }
 
